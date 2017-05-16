745ecaaec5a9ab1bdf02117d70c36fd58ed1e4ce
Josh Hull <j.hull@kunzleigh.com>
2017-05-16 14:05:13 -0400

    Added some rxjs magic

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -3,28 +3,148 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-import { Sorter } from './sorter';
+import 'rxjs/add/operator/concat';
+import 'rxjs/add/operator/switchMap';
+
+import 'rxjs/add/observable/forkJoin';
+
 
 @Injectable()
 export class SwapiService {
   swapiUrl = 'http://swapi.co/api/';
 
-  constructor(private http: Http, private sorter: Sorter) { }
+  constructor(private http: Http) { }
 
-  getPeople() {
-    return this.http.get(this.swapiUrl + 'people/')
-               .map((response: Response) => {
-                    const people = response.json().results;
-                    this.sorter.sort(people, 'name');
-                    return people;
-                 })
-               .catch(this.handleError);
+/* We didn't want you to spend your time paging
+   if you didn't want to.  So we 'Explicitly Configured' the page numbers.
+   The methods here eliminate the hi level resource paging at SWAPI.
+*/
+
+  getAllFilms() {
+    return this.getAllPages([
+      'films/'
+    ]);
   }
 
+  getAllPeople() {
+    return this.getAllPages([
+        'people/'
+      , 'people/?page=2'
+      , 'people/?page=3'
+      , 'people/?page=4'
+      , 'people/?page=5'
+      , 'people/?page=6'
+      , 'people/?page=7'
+      , 'people/?page=8'
+      , 'people/?page=9'
+      ]);
+  }
+
+  getAllPlanets() {
+    return this.getAllPages([
+        'planets/'
+      , 'planets/?page=2'
+      , 'planets/?page=3'
+      , 'planets/?page=4'
+      , 'planets/?page=5'
+      , 'planets/?page=6'
+      , 'planets/?page=7'
+    ]);
+  }
+
+  getAllSpecies() {
+     return this.getAllPages([
+        'species/'
+      , 'species/?page=2'
+      , 'species/?page=3'
+      , 'species/?page=4'
+    ]);
+  }
+
+  getAllStarships() {
+     return this.getAllPages([
+        'starships/'
+      , 'starships/?page=2'
+      , 'starships/?page=3'
+      , 'starships/?page=4'
+     ]);
+  }
+
+  getAllVehicles() {
+    return this.getAllPages([
+        'vehicles/'
+      , 'vehicles/?page=2'
+      , 'vehicles/?page=3'
+      , 'vehicles/?page=4'
+    ]);
+  }
+
+  private getAllPages(pages: string[]) {
+    const urlArray = [];
+    pages.forEach(suffix => urlArray.push(this.swapiUrl + suffix));
+
+    const obsArray = [];
+    urlArray.forEach(url => obsArray.push(this.getResultArrayForPage(url)));
+
+    return Observable.forkJoin(obsArray).map((obsResult: [any[], any[]]) => {
+      let metaResults: any[] = [];
+      obsResult.forEach(results => metaResults = metaResults.concat(results));
+      return metaResults;
+    });
+  }
+
+  // It's kind of fun to watch what happens when you use a merge instead of a forkJoin.
+   getAllPeopleMerge() {
+    return Observable.merge(
+        this.getResultArrayForPage(this.swapiUrl + 'people/')
+      , this.getResultArrayForPage(this.swapiUrl + 'people/?page=2')
+      , this.getResultArrayForPage(this.swapiUrl + 'people/?page=3')
+      , this.getResultArrayForPage(this.swapiUrl + 'people/?page=4')
+      , this.getResultArrayForPage(this.swapiUrl + 'people/?page=5')
+      , this.getResultArrayForPage(this.swapiUrl + 'people/?page=6')
+      , this.getResultArrayForPage(this.swapiUrl + 'people/?page=7')
+      , this.getResultArrayForPage(this.swapiUrl + 'people/?page=8')
+      , this.getResultArrayForPage(this.swapiUrl + 'people/?page=9')
+    );
+  }
+
+  private makeUrlsFromSuffix(suffixes: string[]) {
+    const urlArray: any[] = [];
+    suffixes.forEach(suffix => urlArray.push(this.swapiUrl + suffix));
+    return urlArray;
+  }
+
+  private makeObservables(pages: string[]) {
+    const obsArray: any[] = [];
+    pages.forEach(url => obsArray.push(this.getResultArrayForPage(url)));
+    return obsArray;
+  }
+
+  private getResultArrayForPage(url: string) {
+    return this.http.get(url)
+        .map((response: Response) => {
+            return response.json().results;
+        })
+        .catch(this.handleError);
+  }
+
+  getAllObjectsFor(urls: string[]) {
+    const obsArray = [];
+    urls.forEach(suffix => obsArray.push(this.getObjectFor(suffix)));
+    return Observable.forkJoin(obsArray);
+  }
+
+  getObjectFor(url: string) {
+    return this.http.get(url)
+        .map((response: Response) => {
+            return response.json();
+        })
+        .catch(this.handleError);
+  }
 
   private handleError(error: any) {
       console.error('server error:', error);
-      
+
       if (error instanceof Response) {
         let errMessage = '';
         try {
